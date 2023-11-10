207782e09bab3be1ca06755a9b36383ba926f444
Guilherme M. Miranda <alchemist.software@proton.me>
2023-11-10 18:23:15 -0300

    Added test case for LeetCode 242.

---
+++ src/easy/242.js
@@ -0,0 +1,49 @@
+
+//
+// LeetCode challenge: 242 - Yet unsolved
+//
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+//   Input: s = "anagram", t = "nagaram";
+//   Output: true
+
+// Example 2:
+//   Input: s = "rat", t = "car";
+//   Output: false;
+
+// Constraints:
+//   `1 <= s.length, t.length <= 5 * 10^4`
+//   `s` and `t` consist of lowercase English letters.
+
+import { describe, test } from 'node:test';
+import { strictEqual } from 'node:assert';
+
+/**
+ * @param {string} s
+ * @param {string} t
+ */
+function isAnagram(s, t) {
+  return true;
+}
+
+describe('LeetCode - 242', { timeout: 1_000 }, () => {
+  test('Case 1', () => {
+
+    const firstS = 'anagram';
+    const firstT = 'nagaram';
+
+    strictEqual(isAnagram(firstS, firstT), true);
+
+  });
+  test('Case 2', () => {
+
+    const secondS = 'rat';
+    const secondT = 'car';
+
+    strictEqual(isAnagram(secondS, secondT), false);
+
+  });
+});
