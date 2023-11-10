0e6dd402d694191be9bd88db43766aaa51708725
Guilherme M. Miranda <alchemist.software@proton.me>
2023-11-10 16:49:23 -0300

    Added solution to LeetCode 14.

---
+++ src/easy/leet-14.js
@@ -0,0 +1,70 @@
+
+//
+// LeetCode chalenge: 14 - Solved
+//
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+//   Input:       strs = ["flower", "flow", "flight"];
+//   Output:      "fl"
+
+// Example 2:
+//   Input:       strs = ["dog", "racecar", "car"];
+//   Output:      "";
+//   Explanation: There is no common prefix among the input strings.
+
+// Constraints:
+//   `1 <= strs.length <= 200`
+//   `0 <= strs[i].length <= 200`
+//   `strs[i]` consists of only lowercase English letters.
+
+import { describe, test } from 'node:test';
+import { strictEqual } from 'node:assert';
+
+/**
+ * @param {Array<string>} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+  let result = '';
+
+  for (let index = 0; index < strs[0].length; index++) {
+    if (strs.every((str) => str[index] === strs[0][index])) result += strs[0][index];
+    else break;
+  }
+
+  return result;
+};
+
+describe('LeetCode - 14', { timeout: 1_000 }, () => {
+  test('Case 1', () => {
+
+    const firstInput = ['flower', 'flow', 'flight']
+
+    strictEqual(longestCommonPrefix(firstInput), 'fl');
+
+  });
+  test('Case 2', () => {
+
+    const secondInput = ['dog', 'racecar', 'car'];
+
+    strictEqual(longestCommonPrefix(secondInput), '');
+
+  });
+  test('Case 3', () => {
+
+    const secondInput = [''];
+
+    strictEqual(longestCommonPrefix(secondInput), '');
+
+  });
+  test('Case 4', () => {
+
+    const secondInput = ['a'];
+
+    strictEqual(longestCommonPrefix(secondInput), 'a');
+
+  });
+});
