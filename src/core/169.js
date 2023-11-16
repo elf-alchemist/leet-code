70ec8afdc99400eb22c71e558f1507d2c1452717
Guilherme M. Miranda <alchemist.software@proton.me>
2023-11-16 12:34:51 -0300

    Added test cases for 169

---
+++ src/core/169.js
@@ -0,0 +1,45 @@
+
+//
+// LeetCode 169 - Yet unsolved
+//
+
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times.
+// You may assume that the majority element always exists in the array.
+
+// Example 1:
+//   Input: nums = [3, 2, 3];
+//   Output: 3
+
+// Example 2:
+//   Input: nums = [2, 2, 1, 1, 1, 2, 2];
+//   Output: 2;
+
+// Constraints:
+//   `n == nums.length`
+//   `1 <= n <= 5 * 10^4`
+//   `-10^9 <= nums[i] <= 10^9`
+
+import { describe, test } from 'node:test';
+import { deepStrictEqual } from 'node:assert';
+
+/**
+ * @param {Array<number>} nums
+ * @return {number}
+ */
+function majorityElement(nums) {
+  return Number(nums.some((num) => num))
+};
+
+describe('LeetCode - 169', () => {
+  test('Case 1', () => {
+    const result = majorityElement([3, 2, 3]);
+
+    deepStrictEqual(result, 3);
+  });
+  test('Case 2', () => {
+    const result = majorityElement([2, 2, 1, 1, 1, 2, 2]);
+
+    deepStrictEqual(result, 2);
+  });
+});
