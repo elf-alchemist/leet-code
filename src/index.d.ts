5f4e30c1e5506d882716cf463857d6303753cf9e
Guilherme M. Miranda <alchemist.software@proton.me>
2023-11-20 13:53:47 -0300

    Added more dummy tests

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,11 @@
 import '@total-typescript/ts-reset';
+
+export declare class ListNode {
+  constructor(val: number | undefined, next: ListNode | null | undefined);
+  val: number;
+  next: ListNode | null;
+}
+
+export declare function listToArray(head: ListNode | null): number[];
+
+export declare function listFromArray(array: Array<number>): ListNode;
